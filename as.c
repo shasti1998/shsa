c7fbb3386465aa816a374a09d54a6d4251b8d274
shasti1998 <35728443+shasti1998@users.noreply.github.com>
2018-03-15 16:19:47 +0530

    Create as.c

---
+++ as.c
@@ -0,0 +1,15 @@
+include<stdio.h>
+void main()
+{
+int i,a[10],s;
+printf("enter no of elements in an array\s");
+scanf("%d",&s);
+printf("enter the elements in an array:\s");
+for(i=0;i<s;i++)
+{
+scanf("%d\s",&a[i]);
+}
+for(i=0;i<s;i++)
+{
+printf("a[%d]=%d\n",i,a[i]);
+}
