55e325678ffd39e37ffb13e96f64371ebce91a38
shasti1998 <35728443+shasti1998@users.noreply.github.com>
2018-03-20 16:00:26 +0530

    Rename as.c to aswin.c

---
+++ aswin.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+str1=str(input("Enter string:"));
+char1=0;
+word1=1;
+for i in str1;
+     {char1=char1+1;
+      if(i==' ');
+            word1=word1+1;}
+printf("Number of words in the string:");
+printf(word1);
+printf("Number of characters in the string:");
+printf(char1);
+getch();
+}
